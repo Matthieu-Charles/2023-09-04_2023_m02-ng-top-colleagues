0c87067b3431756c1fd295f00fac8089c7166194
Matthieu Charles <matth.charles@protonmail.com>
2023-09-05 17:34:41 +0200

    feat: voting history (sauf suppression)

--- src/app/shared/components/colleague/colleague.component.ts
+++ src/app/shared/components/colleague/colleague.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Colleague } from 'src/app/models/colleague';
 import { LikeHate } from 'src/app/models/like-hate';
+import { Vote } from 'src/app/models/vote';
 
 @Component({
   selector: 'tc-colleague',
@@ -11,9 +12,16 @@ export class ColleagueComponent {
 
   @Input() colleague!: Colleague;
 
+  @Output() likeOrHateEvent:EventEmitter<Vote> = new EventEmitter<Vote>;
+
   traiter(val :LikeHate) {
     if (val == LikeHate.LIKE) this.colleague.score += 100;
     else this.colleague.score -= 100;
+    let vote :Vote = {
+      colleague: this.colleague,
+      vote: val === "LIKE" ? LikeHate.LIKE : LikeHate.HATE
+    }
+    this.likeOrHateEvent.emit(vote);
   };
 
 }
