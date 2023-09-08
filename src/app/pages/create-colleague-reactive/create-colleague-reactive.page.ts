c8a5d12c5e6acc0b8bc0596454791b398ad794b5
Matthieu Charles <matth.charles@protonmail.com>
2023-09-08 17:26:27 +0200

    fix! compilation problem sccs deleted

--- src/app/pages/create-colleague-reactive/create-colleague-reactive.page.ts
+++ src/app/pages/create-colleague-reactive/create-colleague-reactive.page.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tc-create-colleague-reactive',
   templateUrl: './create-colleague-reactive.page.html',
-  styleUrls: ['./create-colleague-reactive.page.scss']
 })
 export class CreateColleagueReactivePage {
 
