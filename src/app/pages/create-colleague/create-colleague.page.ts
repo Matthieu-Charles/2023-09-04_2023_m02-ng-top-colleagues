c6c855e970384338c0eb1329491d1f3ae2b33920
Matthieu Charles <matth.charles@protonmail.com>
2023-09-11 10:26:36 +0200

    fix! compilation supression lien scss

--- src/app/pages/create-colleague/create-colleague.page.ts
+++ src/app/pages/create-colleague/create-colleague.page.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tc-create-colleague',
   templateUrl: './create-colleague.page.html',
-  styleUrls: ['./create-colleague.page.scss']
 })
 export class CreateColleaguePage {
 
--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -4,8 +4,7 @@ import { Vote } from 'src/app/models/vote';
 @Component({
   selector: 'tc-welcome',
   templateUrl: './welcome.page.html',
-  styleUrls: ['./welcome.page.scss']
 })
 export class WelcomePage {
-  voteArray :Array<Vote> = [];
+  voteArray: Array<Vote> = [];
 }
